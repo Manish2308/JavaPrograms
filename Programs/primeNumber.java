7669038d82fccc07b5cfa38597569d3e91e530eb
Manish2308 <36006744+Manish2308@users.noreply.github.com>
2021-06-05 08:31:38 +0530

    initial

---
+++ Programs/primeNumber.java
@@ -0,0 +1,36 @@
+/*
+Write a program to print to input a number and check whether the number is Prime or not.
+*/
+
+import java.util.Scanner;
+
+public class TestPrimeNumber {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int number;
+		boolean flag = false;
+		
+		System.out.println("Enter the number: ");
+		number = sc.nextInt();
+		
+		                                      //Writing this condition for non-prime numbers.
+		for(int i=2; i<=number; ++i)
+		{
+			if(number % i == 0)
+			{
+				flag = true;
+				break;
+			}
+		}
+		
+		if(!flag)
+			System.out.println(number + " is a Prime Number.");
+		else
+			System.out.println(number + " is not a Prime Number.");
+
+	}
+
+}
