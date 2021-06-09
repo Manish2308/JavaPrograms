0450bc4b32531564bc37a57b14fc775894c07872
Manish Kale <36006744+Manish2308@users.noreply.github.com>
2021-06-09 09:54:50 +0530

    initial

---
+++ Programs/biggestNumbers.java
@@ -0,0 +1,36 @@
+/*
+Write a java program to find the biggest number between 3 numbers.
+*/
+
+import java.util.Scanner;
+public class biggestNumber 
+{
+    public static void main(String[] args) 
+    {
+        int x, y, z;
+        Scanner s = new Scanner(System.in);
+      
+        System.out.print("Enter the first number:");
+        x = s.nextInt();
+      
+        System.out.print("Enter the second number:");
+        y = s.nextInt();
+      
+        System.out.print("Enter the third number:");
+        z = s.nextInt();
+      
+        if(x > y && x > z)
+        {
+            System.out.println("Largest number is:"+x);
+        }
+        else if(y > z)
+        {
+            System.out.println("Largest number is:"+y);
+        }
+        else
+        {
+            System.out.println("Largest number is:"+z);
+        }
+ 
+    }
+}
